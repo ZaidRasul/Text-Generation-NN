58eea806f1df9916601dc55e023fbc0d1e4f9766
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-03 15:52:09 +0500

    file path for training text

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from tensorflow.keras.layers import Dense, Activation, LSTM
 from tensorflow.keras.optimizers import RMSprop
 import numpy as np
 
+filepath = tf.keras.utils.get_file("shakespeare.txt", "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt")
