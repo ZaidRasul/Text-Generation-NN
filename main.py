1f5949ed97e4871c320db782db539a483c8b7584
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-03 23:06:37 +0500

    sample + char to index mapping

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, LSTM
 from tensorflow.keras.optimizers import RMSprop
 import numpy as np
+
 # get the text file for training.
 filepath = tf.keras.utils.get_file("shakespeare.txt", "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt")
 text = open(filepath, 'rb').read().decode(encoding='utf-8').lower() #open the file , read it, decode it to utf-8 and convert to lower case 
+
 # taking sample of text for training
 text = text[50000:150000]
+characters = sorted(set(text)) # get the unique characters in the text and sort them
+
+# create a dictionary that maps each character to an index
+char_index = dict((char, index) for index, char in enumerate(characters))
