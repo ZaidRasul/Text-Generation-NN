902dd49aa6d219bfc4f10f709cc9629ecb4d509d
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-03 15:19:08 +0500

    initial imports

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, LSTM
+from tensorflow.keras.optimizers import RMSprop
+import numpy as np
+
